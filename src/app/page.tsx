bf164a5c2e400fec59b5ac6db99d9f15196f61c0
iinava <131179814+iinava@users.noreply.github.com>
2024-11-15 15:34:51 +0530

    feat: :sparkles: add all sections in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import Navbar from "@/components/Navbar";
+import AchievementsSection from "@/components/sections/AchievementsSection";
+import CalenderSection from "@/components/sections/CalenderSection";
 import ClientSection from "@/components/sections/ClientSection";
 import CommunitySection from "@/components/sections/CommunitySection";
+import FooterSection from "@/components/sections/FooterSection";
 import HeroSection from "@/components/sections/HeroSection";
+import MarketingSection from "@/components/sections/MarketingSection";
+import TestimonialSection from "@/components/sections/TestimonialSection";
+import UnlockSection from "@/components/sections/UnlockSection";
 
 export default function Home() {
   return (
-   <div>
+   <>
     <Navbar/>
     <HeroSection/>
     <ClientSection/>
     <CommunitySection/>
-   </div>
+    <UnlockSection/>
+    <AchievementsSection/>
+    <CalenderSection/>
+    <TestimonialSection/>
+    <MarketingSection/>
+    <FooterSection/>
+   </>
   );
 }
